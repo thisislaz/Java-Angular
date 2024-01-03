52eb4e7125f7f09ef325e56ef6266267d7b4f3cc
Lazaro Alvarez <99672593+thisislaz@users.noreply.github.com>
2024-01-03 01:16:41 -0500

    first commit

---
+++ src/main/java/net/thisislaz/angularwithspringboot/AngularWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.thisislaz.angularwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularWithSpringBootApplication.class, args);
+	}
+
+}
